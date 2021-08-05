2c1966ab7ef9317231ee6cd76e5b015953a1e9a0
Wilder Medeiros <wilder.medeiros@outlook.com>
2021-08-05 13:29:54 -0300

    13 - Tentando refatorar a linguagem para favorecer algumas funcionalidades

---
+++ src/recovery/First.java
@@ -0,0 +1,16 @@
+package recovery;
+
+import parser.*;
+
+public class First 
+{ //implementa os conjuntos first p/ alguns n.terminais
+
+    static public final RecoverySet main = new RecoverySet();
+
+    static 
+    {
+    	main.add(new Integer(FreshMeatConstants.EOF));
+    	
+    	
+    }
+}
